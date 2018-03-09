d888e3dd5ee0af6813696b46fa0941c6ab4394c5
Grayson Bennion <graysonbennion@gmail.com>
2018-03-09 14:52:32 -0700

    Added changes from wiki

--- public/script.js
+++ public/script.js
@@ -1,64 +1,99 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    items: [],
-    text: '',
-    show: 'all',
-    drag: {},
-  },
-  computed: {
-    activeItems: function() {
-      return this.items.filter(function(item) {
-	return !item.completed;
-      });
+    el: '#app',
+    data: {
+	items: [],
+	text: '',
+	show: 'all',
+	drag: {}
     },
-    filteredItems: function() {
-      if (this.show === 'active')
-	return this.items.filter(function(item) {
-	  return !item.completed;
-	});
-      if (this.show === 'completed')
-	return this.items.filter(function(item) {
-	  return item.completed;
-	});
-      return this.items;
+    
+    created: function() {
+	this.getItems();
     },
-  },
-  methods: {
-    addItem: function() {
-      this.items.push({text: this.text,completed:false});
-      this.text = '';
+    
+    computed: {
+	activeItems: function() {
+	    return this.items.filter(function(item) {
+		return !item.completed;
+	    });
     },
-    completeItem: function(item) {
-      item.completed = !item.completed;
+	filteredItems: function() {
+	    if (this.show === 'active')
+		return this.items.filter(function(item) {
+		    return !item.completed;
+		});
+	    if (this.show === 'completed')
+		return this.items.filter(function(item) {
+		    return item.completed;
+		});
+	    return this.items;
+	},
     },
-    deleteItem: function(item) {
-      var index = this.items.indexOf(item);
-      if (index > -1)
-	this.items.splice(index,1);
-    },
-    showAll: function() {
-      this.show = 'all';
-    },
-    showActive: function() {
-      this.show = 'active';
-    },
-    showCompleted: function() {
-      this.show = 'completed';
-    },
-    deleteCompleted: function() {
-      this.items = this.items.filter(function(item) {
-	return !item.completed;
-      });
-    },
-    dragItem: function(item) {
-      this.drag = item;
-    },
-    dropItem: function(item) {
-      var indexItem = this.items.indexOf(this.drag);
-      var indexTarget = this.items.indexOf(item);
-      this.items.splice(indexItem,1);
-      this.items.splice(indexTarget,0,this.drag);
-    },
-  }
+    methods: {
+	addItem: function() {
+	    axios.post("http://localhost:3000/api/items", {
+		text: this.text,
+		completed: false
+	    }).then(response => {
+		this.text = "";
+		this.getItems();
+		return true;
+	    }).catch(err => {
+	    });
+	},
+	completeItem: function(item) {
+	    axios.put("http://localhost:3000/api/items/" + item.id, {
+		text: item.text,
+		completed: !item.completed,
+		orderChange: false,
+	    }).then(response => {
+		return true;
+	    }).catch(err => {
+	    });
+	},
+	deleteItem: function(item) {
+	    axios.delete("http://localhost:3000/api/items/" + item.id).then(response => {
+		this.getItems();
+		return true;
+	    }).catch(err => {
+	    });
+	},
+	showAll: function() {
+	    this.show = 'all';
+	},
+	showActive: function() {
+	    this.show = 'active';
+	},
+	showCompleted: function() {
+	    this.show = 'completed';
+	},
+	deleteCompleted: function() {
+	    this.items.forEach(item => {
+		if (item.completed)
+		    this.deleteItem(item)
+	    });
+	},
+	dragItem: function(item) {
+	    this.drag = item;
+	},
+	dropItem: function(item) {
+	    axios.put("http://localhost:3000/api/items/" + this.drag.id, {
+		text: this.drag.text,
+		completed: this.drag.completed,
+		orderChange: true,
+		orderTarget: item.id
+	    }).then(response => {
+		this.getItems();
+		return true;
+	    }).catch(err => {
+	    });
+	},
+	getItems: function() {
+	    axios.get("http://localhost:3000/api/items").then(response => {
+		this.items = response.data;
+		return true;
+	    }).catch(err => {
+	    });
+	},
+    }
 });
