534b36ef23546bed055f8a779369442b31f74046
Grayson Bennion <graysonbennion@gmail.com>
2018-03-10 14:20:51 -0700

    added changes to CORS exception

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.use(express.static('public'))
 let items = [];
 let id = 0;
 
-app.get('/api/items', (req, res) => {
-    res.send(items);
-});
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
+});
+
+app.get('/api/items', (req, res) => {
+    res.send(items);
 });
 
 app.post('/api/items', (req, res) => {
